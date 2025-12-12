95c0b868c3a6eef30e6031874edd59513501c48f
Aditya Nugraha <adityanugraha@students.usu.ac.id>
2025-12-12 20:52:17 +0700

    Add input functions and efficiency analysis

--- Analisis konsumsi BMM.pas
+++ Analisis konsumsi BMM.pas
@@ -92,3 +92,95 @@ begin
   until x >= 0;
   inputHari := x;
 end;
+
+function inputJam: integer;
+var x: integer;
+begin
+  repeat
+    write('Jam perjalanan (0-23): '); readln(x);
+    if (x < 0) or (x > 23) then writeln('Jam harus antara 0 sampai 23!');
+  until (x >= 0) and (x <= 23);
+  inputJam := x;
+end;
+
+function inputJarak: real; var r: real; begin write('Jarak (km): '); readln(r); inputJarak := r; end;
+function inputBahanBakar: real; var r: real; begin write('Bahan bakar (liter): '); readln(r); inputBahanBakar := r; end;
+
+function analisisCC(cc: integer): string;
+begin
+  if cc < 150 then analisisCC := 'Tenaga rendah, sangat irit'
+  else if cc < 1000 then analisisCC := 'Tenaga sedang, cukup irit'
+  else if cc < 2000 then analisisCC := 'Tenaga menengah, konsumsi wajar'
+  else if cc < 3000 then analisisCC := 'Tenaga besar, cenderung boros'
+  else analisisCC := 'Tenaga sangat besar, boros BBM';
+end;
+
+function kategoriEfisiensi(kmpl: real): string;
+begin
+  if kmpl >= 15 then kategoriEfisiensi := 'Sangat Efisien'
+  else if kmpl >= 10 then kategoriEfisiensi := 'Cukup Efisien'
+  else kategoriEfisiensi := 'Boros';
+end;
+
+procedure inputData(var k: kendaraan);
+const
+  jenisList: array[0..1] of string = ('RODA 2','RODA 4');
+  merekMotor: array[0..4] of string = ('HONDA','YAMAHA','SUZUKI','KAWASAKI','LAINNYA');
+  merekMobil: array[0..5] of string = ('TOYOTA','HONDA','DAIHATSU','MITSUBISHI','SUZUKI','LAINNYA');
+  bbmList: array[0..6] of string = (
+    'PERTALITE','PERTAMAX','PERTAMAX TURBO',
+    'SOLAR SUBSIDI','BIO SOLAR','DEXLITE','DEX'
+  );
+  bbmHarga: array[0..6] of integer = (
+    10000,12500,14000,6800,7000,13000,14500
+  );
+var totalJam: real; idx: integer;
+begin
+  clrscr;
+  writeln('--- INPUT DATA PERJALANAN ---');
+
+  idx := swipeMenu('PILIH JENIS KENDARAAN', jenisList);
+  k.jenisKendaraan := jenisList[idx];
+
+
+  if k.jenisKendaraan = 'RODA 2' then
+    idx := swipeMenu('PILIH MEREK RODA 2', merekMotor)
+  else
+    idx := swipeMenu('PILIH MEREK RODA 4', merekMobil);
+
+  if ((k.jenisKendaraan='RODA 2') and (merekMotor[idx]='LAINNYA'))
+     or ((k.jenisKendaraan='RODA 4') and (merekMobil[idx]='LAINNYA')) then
+  begin
+    write('Masukkan merek manual: '); readln(k.merek);
+  end else begin
+    if k.jenisKendaraan='RODA 2' then k.merek := merekMotor[idx]
+    else k.merek := merekMobil[idx];
+  end;
+
+  k.ccMesin := inputCC;
+  k.kategoriCC := analisisCC(k.ccMesin);
+
+ 
+  idx := swipeMenuVertical('PILIH JENIS BBM', bbmList, bbmHarga);
+  k.jenisBBM := bbmList[idx];
+  k.harga := bbmHarga[idx];
+
+  k.hari := inputHari;
+  k.jam := inputJam;
+  k.jarak := inputJarak;
+  k.bahanBakar := inputBahanBakar;
+
+  if k.bahanBakar > 0 then k.konsumsi := k.jarak / k.bahanBakar else k.konsumsi := 0;
+  k.biaya := k.bahanBakar * k.harga;
+
+  totalJam := (k.hari * 24) + k.jam;
+  if k.bahanBakar > 0 then begin
+    k.waktuPerLiterJam := totalJam / k.bahanBakar;
+    k.waktuPerLiterMenit := k.waktuPerLiterJam * 60;
+  end else begin
+    k.waktuPerLiterJam := 0;
+    k.waktuPerLiterMenit := 0;
+  end;
+
+  k.kategoriEfisiensi := kategoriEfisiensi(k.konsumsi);
+end;
